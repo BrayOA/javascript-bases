56cef15ffae913ce4e85cd8f96465cffdb799ed3
Brayan <borconarellano@gmail.com>
2023-01-25 16:17:21 -0500

    finish for

--- 01.fundamentos/assets/js/ciclos/for.js
+++ 01.fundamentos/assets/js/ciclos/for.js
@@ -2,3 +2,17 @@
 const heroes = ['Batman', 'Superman', 'Mujer Maravilla','Aquaman'];
 
 
+console.warn('for tradicional');
+for (let i = 0; i < heroes.length; i++) {
+console.log(heroes[i]);
+}
+
+console.warn ('for in');
+for (let i in heroes) {
+    console.log(heroes[i]);
+}
+
+console.warn ('for of');
+for (let heroe of heroes) {
+    console.log(heroe);
+}
