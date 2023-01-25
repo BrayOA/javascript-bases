642cb11f2229dd1613f685e851730026ed232add
Brayan <borconarellano@gmail.com>
2023-01-24 23:38:11 -0500

    for

---
+++ 01.fundamentos/assets/js/ciclos/while-do-while.js
@@ -0,0 +1,35 @@
+
+const carros = ['ford', 'mazda', 'toyota'];
+
+let i = 0;
+
+// while (i < carros.length) {
+//     console.log(carros[i]);
+//     // i = i +1;
+//     i++;
+// }
+
+// undefined
+// null
+// false
+
+while (carros[i]) {
+    if ( i === 1) {
+        // break;
+        i++;
+        continue;
+    }
+    console.log(carros[i]);
+    // i = i +1;
+    i++;
+}
+
+
+console.warn('do while');
+let j = 0;
+do{
+    console.log(carros[j]);
+    j++;
+}while(carros[j]);
+
+
