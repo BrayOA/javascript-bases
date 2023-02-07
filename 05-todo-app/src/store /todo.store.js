bcc3de2c1ef7349f5ec2913bf9387961b248682c
Brayan <borconarellano@gmail.com>
2023-02-06 23:53:43 -0500

    todosStore

--- 05-todo-app/src/store /todo.store.js
+++ 05-todo-app/src/store /todo.store.js
@@ -20,6 +20,70 @@ const initStore = () => {
     console.log('InitStore 🥑');
 }
 
+
+const loadStore = ()=>{
+    throw Error ('not implemented');
+}
+
+const getTodos = (filter = Filters.All)=>{
+    throw Error ('not implemented');
+}
+
+
+
+/**
+ *
+ * @param {String} description
+ */
+
+
+const addstore = (description)=>{
+    throw Error ('not implemented');
+
+}
+
+/**
+ *
+ * @param {String} todoId
+ */
+const toggleTodo = (todoId)=>{
+    throw Error ('not implemented');
+
+}
+
+const deleteTodo = (todoId)=>{
+    throw Error ('not implemented');
+
+}
+
+const deleteCompleted = ()=>{
+    throw Error ('not implemented');
+
+}
+
+const setFilter = (newFilter= Filters.All)=>{
+    throw Error ('not implemented');
+
+}
+
+const getCurrentFilter = ()=>{
+    throw Error ('not implemented');
+
+}
+
+
+
+
+
+
+
+
 export default {
     initStore,
+    loadStore,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter,
 }
