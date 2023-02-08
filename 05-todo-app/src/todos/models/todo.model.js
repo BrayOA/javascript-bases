fc4f0eebc9f5935a69d9fdc27ceaafa9adb09869
Brayan <borconarellano@gmail.com>
2023-02-08 18:34:08 -0500

    main

--- 05-todo-app/src/todos/models/todo.model.js
+++ 05-todo-app/src/todos/models/todo.model.js
@@ -1,18 +1,16 @@
-import {v4 as uuid} from 'uuid';
-
+import { v4 as uuid } from 'uuid';
 
 export class Todo {
 
     /**
-     *
-     * @param {String} description
+     * 
+     * @param {String} description 
      */
-
-    constructor (description){
+    constructor( description ){
         this.id = uuid();
-        this.descripcion = description;
+        this.description = description;
         this.done = false;
-        this.createdAt =  new Date();
+        this.createdAt = new Date();        
     }
 
 }
