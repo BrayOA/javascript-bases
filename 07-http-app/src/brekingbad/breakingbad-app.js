4327764434e30f6a2b80213e7cb9547cc062e52b
Brayan <borconarellano@gmail.com>
2023-02-11 05:39:29 -0500

    api

---
+++ 07-http-app/src/brekingbad/breakingbad-app.js
@@ -0,0 +1,12 @@
+
+
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+
+export const BreikingbadApp = (element) => {
+    document.querySelector('#app-title').innerHTML ='Breakingbad App';
+    element.innerHTML= 'Loading...'
+
+}
